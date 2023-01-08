e73eb1bc803a4ffd9f891c42fc75a8531fb5bf0c
Nikola <djindjojlo@gmail.com>
2023-01-08 01:52:30 +0100

    eo

--- DEV.tab/DEV.panel/Floor and roof by room.pushbutton/script.py
+++ DEV.tab/DEV.panel/Floor and roof by room.pushbutton/script.py
@@ -1,14 +1,19 @@
 # This Python file uses the following encoding: utf-8
 import clr
+from Autodesk.Revit import Exceptions, Creation
 from Autodesk.Revit.DB import Element, XYZ, CurveArray, ModelCurveArray, SpatialElementBoundaryOptions
 from pyrevit import revit, DB, script, HOST_APP
 from pyrevit.revit.db import query
+from pyrevit.framework import List
 from rpw.ui.forms import FlexForm, Label, TextBox, Button, ComboBox, Separator, CheckBox
-from Autodesk.Revit import Exceptions, Creation
+doc = revit.doc
+version = HOST_APP.version
+
+
 
 def find_by_class_and_name(dbItem, namez):
 
-    findBy = DB.FilteredElementCollector(revit.doc).OfClass(getattr(DB, dbItem)).ToElements()
+    findBy = DB.FilteredElementCollector(doc).OfClass(getattr(DB, dbItem)).ToElements()
     if findBy:
         return [find for find in findBy if find.Name == namez]
     else:
@@ -18,16 +23,16 @@ def find_by_class_and_name(dbItem, namez):
 # print(find_by_class_and_name("RoofType", "600x600 Armstrong"))
 
 #kolektor
-levels = DB.FilteredElementCollector(revit.doc).OfClass(DB.Level).ToElements()
-roofTypes = DB.FilteredElementCollector(revit.doc).OfClass(DB.RoofType).ToElements()
-floors = DB.FilteredElementCollector(revit.doc).OfClass(DB.FloorType).ToElements()
+levels = DB.FilteredElementCollector(doc).OfClass(DB.Level).ToElements()
+roofTypes = DB.FilteredElementCollector(doc).OfClass(DB.RoofType).ToElements()
+floors = DB.FilteredElementCollector(doc).OfClass(DB.FloorType).ToElements()
 
-rooms = DB.FilteredElementCollector(revit.doc).OfCategory(DB.BuiltInCategory.OST_Rooms).WhereElementIsNotElementType().ToElements()
-roofs = DB.FilteredElementCollector(revit.doc).OfCategory(DB.BuiltInCategory.OST_Roofs).WhereElementIsNotElementType().ToElements()
-svaGledista = (DB.FilteredElementCollector(revit.doc).OfClass(DB.ViewFamilyType)) 
+rooms = DB.FilteredElementCollector(doc).OfCategory(DB.BuiltInCategory.OST_Rooms).WhereElementIsNotElementType().ToElements()
+roofs = DB.FilteredElementCollector(doc).OfCategory(DB.BuiltInCategory.OST_Roofs).WhereElementIsNotElementType().ToElements()
+svaGledista = (DB.FilteredElementCollector(doc).OfClass(DB.ViewFamilyType)) 
 floor_plan_type = [vt for vt in svaGledista.WhereElementIsElementType() if vt.FamilyName == "Floor Plan"][1]
 
-osnova = DB.FilteredElementCollector(revit.doc).OfClass(DB.View).ToElements()
+osnova = DB.FilteredElementCollector(doc).OfClass(DB.View).ToElements()
 osnova = [find for find in osnova if find.Name == "±0.00"][0]
 
 #dict
@@ -89,19 +94,21 @@ if a == True:
 else:
     chosen_crop_offset = 0
 
-with revit.Transaction("Create roof and floor", revit.doc):
-    
-    room_boundary_options = SpatialElementBoundaryOptions()
+with revit.Transaction("Create roof and floor", doc): 
+
     for room in rooms:
+        room_boundary = room.GetBoundarySegments(SpatialElementBoundaryOptions())
+        #floor_shape = room_boundary[0]
+        openings = list(room_boundary)[1:] if len(room_boundary) > 1 else []
         
         if room.Area > 0:
             room_level_id = room.Level
-            room_boundary = room.GetBoundarySegments(room_boundary_options)[0]
+            room_boundary = room.GetBoundarySegments(room_boundary)[0]
             room_curves = CurveArray()
             roof_curves = DB.CurveLoop()
             normal = XYZ.BasisZ
                         
-            for boundary_segment in room_boundary:
+            for boundary_segment in room_boundary[0]:
                 crv = boundary_segment.GetCurve()
                 room_curves.Append(crv)
                 roof_curves.Append(crv)
@@ -114,10 +121,80 @@ with revit.Transaction("Create roof and floor", revit.doc):
                     roofCurveArray.Append(c)
                 
             if chosen_place_floor: #create floors
-                newFlor = revit.doc.Create.NewFloor(room_curves, chosen_floor, chosen_floor_level, False, normal )
-                flOffsetFromLvl = newFlor.get_Parameter(DB.BuiltInParameter.FLOOR_HEIGHTABOVELEVEL_PARAM).Set(0)
-            
+                
+                if version < 2023:
+                    newFlor = doc.Create.NewFloor(room_curves, chosen_floor, chosen_floor_level, False, normal )
+                    flOffsetFromLvl = newFlor.get_Parameter(DB.BuiltInParameter.FLOOR_HEIGHTABOVELEVEL_PARAM).Set(0)
+                    if openings:
+                        for opening in openings:
+                            opening_curve = CurveArray()
+                            for seg in opening:
+                                opening_curve.Append(seg.GetCurve())
+                    floor_opening = doc.Create.NewOpening(newFlor, opening_curve, True)
+                        
+                if version >= 2023:
+                    List_curve_loop = List[DB.CurveLoop]()
+                    for room_outline in room_boundary:
+                        curve_loop = DB.CurveLoop()
+                        for seg in room_outline:
+                            curve_loop.Append(seg.GetCurve())
+                        List_curve_loop.Add(curve_loop)
+                    newFlor = DB.Floor.Create(doc, room_curves, chosen_floor, chosen_floor_level, False, normal )
+                    flOffsetFromLvl = newFlor.get_Parameter(DB.BuiltInParameter.FLOOR_HEIGHTABOVELEVEL_PARAM).Set(0)
+                
             if chosen_place_roof: #create roofs
                 mcArray = clr.StrongBox[ModelCurveArray](ModelCurveArray())		
-                newRof = revit.doc.Create.NewFootPrintRoof(roofCurveArray, chosen_roof_level, chosen_roof_type, mcArray)
+                newRof = doc.Create.NewFootPrintRoof(roofCurveArray, chosen_roof_level, chosen_roof_type, mcArray)
                 rfOffsetFromLvl = newRof.get_Parameter(DB.BuiltInParameter.ROOF_LEVEL_OFFSET_PARAM).Set(32/foot)
+                
+                
+                
+
+def room_to_floor(room, floor_type):
+    # Make sure that Room is bounding.
+    if not room.get_Parameter(DB.BuiltInParameter.ROOM_AREA).AsDouble():
+        return None
+
+    # >>>>>>>>>> CREATE FLOOR
+    with revit.Transaction("Create roof and floor", doc): 
+
+        # >>>>>>>>>> ROOM BOUNDARIES
+        room_boundaries = room.GetBoundarySegments(SpatialElementBoundaryOptions())
+        floor_shape = room_boundaries[0]
+        openings = list(room_boundaries)[1:] if len(room_boundaries) > 1 else []
+
+        if version < 2023:
+            curve_array = CurveArray()
+            for seg in floor_shape:
+                curve_array.Append(seg.GetCurve())
+            new_floor = doc.Create.NewFloor(curve_array, chosen_floor, chosen_floor_level, False)
+
+            # >>>>>>>>>> CREATE FLOOR OPENINGS SEPERATELY BEFORE RVT 2023
+            if openings:
+                with revit.Transaction("Create Openings", doc): 
+                    for opening in openings:
+                        opening_curve = CurveArray()
+                        for seg in opening:
+                            opening_curve.Append(seg.GetCurve())
+                        floor_opening = doc.Create.NewOpening(new_floor, opening_curve, True)
+
+        if version >= 2023:
+            List_curve_loop = List[DB.CurveLoop]()
+            for room_outline in room_boundaries:
+                curve_loop = DB.CurveLoop()
+                for seg in room_outline:
+                    curve_loop.Append(seg.GetCurve())
+                List_curve_loop.Add(curve_loop)
+            new_floor = DB.Floor.Create(doc, List_curve_loop, chosen_floor.Id, chosen_floor_level.Id) #FIXME
+
+    return new_floor
+#
+def create_floors(selected_rooms, chosen_floor):
+    """Function to loop through selected rooms and create floors from them."""
+    # >>>>>>>>>> LOOP THROUGH ROOMS
+    with TransactionGroup(doc, __title__) as tg:
+        tg.Start()
+        for r in selected_rooms:
+            with try_except(debug=True):
+                room_to_floor(room = r, floor_type=chosen_floor)
+        tg.Assimilate()
