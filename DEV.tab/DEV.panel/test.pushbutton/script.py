b07b068be2650ae3ae59ad6708edc662e95fc5ff
Nikola <djindjojlo@gmail.com>
2023-05-07 01:03:42 +0200

    clear mark restrictions

--- DEV.tab/DEV.panel/test.pushbutton/script.py
+++ DEV.tab/DEV.panel/test.pushbutton/script.py
@@ -1,26 +1,38 @@
-from pyrevit import revit, DB
+from pyrevit import revit, DB, forms
 
 from Autodesk.Revit import *
 
 
-doc = revit.doc
+host = revit.HOST_APP.username
 
-rooms = DB.FilteredElementCollector(doc)\
-    .OfCategory(DB.BuiltInCategory.OST_Rooms)\
-    .WhereElementIsNotElementType()
 
-tolerance = 0.15
+if host == "bob" or host == "marley":
+    print (host)
+else:
+    forms.alert(title="Restricted!", 
+                msg="Not allowed for the current user", 
+                sub_msg="Contact your BIM manager for access", 
+                expanded="This option can break your current Marks. \nAfter Marking, the marks probably wont be the same as they were for each panel! ", 
+                warn_icon=True)
 
-for room in rooms:
-    corner_count = 0
-    room_name = room.get_Parameter(DB.BuiltInParameter.ROOM_NAME).AsString()
-    room_boundary = room.GetBoundarySegments(DB.SpatialElementBoundaryOptions())
-    for boundary in room_boundary:
-        for segment in boundary:
-            start_point = segment.GetCurve().GetEndPoint(0)
-            end_point = segment.GetCurve().GetEndPoint(1)
-            # check if the start and end points are different and close to each other
-            if start_point != end_point and start_point.IsAlmostEqualTo(end_point, tolerance):
-                corner_count += 1
-    print('Room: ' + room_name)
-    print('Number of corners: ' + str(corner_count))
+# doc = revit.doc
+
+# rooms = DB.FilteredElementCollector(doc)\
+#     .OfCategory(DB.BuiltInCategory.OST_Rooms)\
+#     .WhereElementIsNotElementType()
+
+# tolerance = 0.15
+
+# for room in rooms:
+#     corner_count = 0
+#     room_name = room.get_Parameter(DB.BuiltInParameter.ROOM_NAME).AsString()
+#     room_boundary = room.GetBoundarySegments(DB.SpatialElementBoundaryOptions())
+#     for boundary in room_boundary:
+#         for segment in boundary:
+#             start_point = segment.GetCurve().GetEndPoint(0)
+#             end_point = segment.GetCurve().GetEndPoint(1)
+#             # check if the start and end points are different and close to each other
+#             if start_point != end_point and start_point.IsAlmostEqualTo(end_point, tolerance):
+#                 corner_count += 1
+#     print('Room: ' + room_name)
+#     print('Number of corners: ' + str(corner_count))
